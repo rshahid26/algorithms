16236f034bcfa89539c99b471f68063f4517f4b3
rshahid26 <114392205+rshahid26@users.noreply.github.com>
2023-11-14 00:28:25 -0500

    rotation methods with diagram

--- data_structures/AVLTree.py
+++ data_structures/AVLTree.py
@@ -49,25 +49,79 @@ class AVLTree(RecursiveBST):
         if node.bf() > self.MAX_IMBALANCE:
             # Right subtree leans right. Decrement RR height, RL becomes LR
             if node.right.bf() >= 0:
+                #
                 self.rotate_left(node)
-            # Right subtree leans left.
+            # Right subtree leans left. Rotate the right subtree to lean right first.
             else:
                 self.rotate_right(node.right)
                 self.rotate_left(node)
 
-        # Left subtree too long
-        elif node.bf() < -self.MAX_IMBALANCE:
-            # Left subtree leans left. Decrement LL height, LR becomes RL
+        # Left subtree is too long
+        if node.bf() < -self.MAX_IMBALANCE:
+            # Left subtree leans left. Decrement LL height, LR becomes RL.
             if node.left.bf() <= 0:
                 self.rotate_right(node)
-            # Left subtree leans right.
+            # Left subtree leans right. Rotate the left subtree to lean left first.
             else:
                 self.rotate_left(node.left)
                 self.rotate_right(node)
 
-
     def rotate_left(self, node: AVLNode):
-        pass
+        #   Before:    |      After:        |  x,y, and z necessarily exist to trigger rotate_left.
+        #      |       |        |           |  every edge in the diagram is bidirectional.
+        #      x       |        y           |
+        #       \      |       / \          |  temp may or may not exist. this is only relevant when
+        #        y     |      x   z         |  setting temp's parent to x.
+        #       / \    |       \            |
+        #   temp   z   |      temp          |  note that z and y.right is unchanged.
+
+        x, y = node, node.right
+
+        temp = y.left
+        y.left = x
+        x.right = temp
+
+        y.parent = x.parent
+        x.parent = y
+        if temp:
+            temp.parent = x
+
+        # Now that y is the root of the subtree, make y's parent point to y
+        if y.parent is None: # x was a root
+            self.root = y
+        elif y.parent == y.parent.left:  # x was a left child
+            y.parent.left = y
+        else:  # x was a right child
+            y.parent.right = y
+        x.height = 1 + max(self.height(x.left), self.height(x.right))
+        y.height = 1 + max(self.height(y.left), self.height(y.right))
 
     def rotate_right(self, node: AVLNode):
-        pass
+        #   Before:    |      After:        |  x, y, and z necessarily exist to trigger rotate_right.
+        #      |       |        |           |  every edge in the diagram is bidirectional.
+        #      x       |        y           |
+        #     /        |       / \          |  temp may or may not exist. this is only relevant when
+        #    y         |      z   x         |  setting temp's parent to x.
+        #   / \        |         /          |
+        #  z   temp    |        temp        |  note that z and y.left is unchanged.
+
+        x, y = node, node.left
+
+        temp = y.right
+        y.right = x
+        x.left = temp
+
+        y.parent = x.parent
+        x.parent = y
+        if temp:
+            temp.parent = x
+
+        # Now that y is the root of the subtree, make y's parent point to y
+        if y.parent is None: # x was the root
+            self.root = y
+        elif y.parent.left == x:  # x was a left child
+            y.parent.left = y
+        else:  # x was a right child
+            y.parent.right = y
+        x.height = 1 + max(self.height(x.left), self.height(x.right))
+        y.height = 1 + max(self.height(y.left), self.height(y.right))
